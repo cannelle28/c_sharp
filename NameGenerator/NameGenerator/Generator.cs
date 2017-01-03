d42503eea0df3bce29500d4551f24e9c86cef1e3
cannelle28 <wioletta.manka728@gmail.com>
2017-01-04 00:40:51 +0100

    Add files via upload

---
+++ NameGenerator/NameGenerator/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameGenerator
+{
+    public class Generator
+    {
+        string rnd, rndSurname;
+        string x;
+        public string[] maleName = { "Aiden", "Lucas", "Liam", "Noah", "Ethan", "Jacob", "Grayson", "James", "Michael", "Daniel", "Andrew", "Ryan" }; 
+        public string[] femaleName = { "Sophia", "Emma", "Olivia", "Mia", "Aria", "Zoey", "Lily", "Emily", "Harper", "Chloe", "Victoria", "Scarlett" };
+        public string[] surname = { "Smith", "Jackson", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Harris", "Martinez", "Young", "Hill" };
+        Random random = new Random();
+
+        public void generate(char gender)
+        {
+            if (gender.Equals('m'))
+            {
+                rnd = maleName[random.Next(maleName.Length)]; // losowanie stringa z tablicy maleName[]
+                x = "male";
+            }
+            else if (gender.Equals('f'))
+            {
+                rnd = femaleName[random.Next(femaleName.Length)];
+                x = "female";
+            }
+            rndSurname = surname[random.Next(surname.Length)];
+
+            Console.Write("Random " + x + " name: " + rnd + " " + rndSurname);
+        }
+    }
+}
---
+++ NameGenerator/NameGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genderChosen;
+            Console.WriteLine("NAME GENERATOR");
+            Console.WriteLine("Select your gender.\nPress:\n1. 'm' for male\n2. 'f' for female\n3. 'q' to quit program\n");
+
+            do
+            {
+                genderChosen = Console.ReadKey().KeyChar; // wczytywanie znaku od uzytkownika do zmiennej
+                if (!(genderChosen.Equals('f') || genderChosen.Equals('m') || genderChosen.Equals('q')))
+                {
+                    ClearCurrentConsoleLine();
+                    Console.Write("You pressed a wrong char, try again");
+                    System.Threading.Thread.Sleep(1000); // zatrzymanie dzialania na 1 sek
+                    ClearCurrentConsoleLine();
+                }
+                else if ((genderChosen.Equals('f') || genderChosen.Equals('m')))
+                {
+                    ClearCurrentConsoleLine();
+                    Generator gen = new Generator();
+                    gen.generate(genderChosen);
+                }
+                else if (genderChosen.Equals('q'))
+                {
+                    System.Diagnostics.Process.GetCurrentProcess().Kill(); // wylaczanie aplikacji
+                }
+            } while (!(genderChosen.Equals('q')));
+        }
+
+        // clear current line
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+    }
+}
