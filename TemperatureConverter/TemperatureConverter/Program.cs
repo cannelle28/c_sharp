3dd8d89b465e68c17dc3b2053e20d14f4cf2f556
cannelle28 <wioletta.manka728@gmail.com>
2017-01-26 22:34:43 +0100

    Add files via upload

---
+++ TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            TempConverter converter = new TempConverter();
+
+            Console.WriteLine("TEMPERATURE CONVERTER\n");
+            Console.WriteLine("1. Celsius -> Fahrenheit\n2. Fahrenheit -> Celsius\n");
+            do
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (!(choice.Equals(1) || choice.Equals(2))) Console.WriteLine("Wrog input");
+            } while (!(choice.Equals(1) || choice.Equals(2)));
+
+            if (choice.Equals(1))
+            {
+                converter.readTemp();
+                Console.WriteLine(converter.convertTempToF() + " F");
+            }
+            else
+            {
+                converter.readTemp();
+                Console.WriteLine(converter.convertTempToC() + " C");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConverter/TempConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    class TempConverter
+    {
+        public double temperature;
+
+        public void readTemp()
+        {
+            Console.WriteLine("Enter temperature: ");
+            temperature = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public double convertTempToF()
+        {
+            return (temperature * 9/5+32);
+        }
+        public double convertTempToC()
+        {
+            return (temperature - 32) * 5 / 9;
+        }
+    }
+
+}
